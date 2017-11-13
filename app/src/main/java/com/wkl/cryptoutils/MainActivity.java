80929e77dc8656944630ceea2fb431056cce4494
wkl <690340436@qq.com>
2017-11-13 19:44:11 +0800

    正在搜集资料，支持到Api14

--- app/src/main/java/com/wkl/cryptoutils/MainActivity.java
+++ app/src/main/java/com/wkl/cryptoutils/MainActivity.java
@@ -3,11 +3,24 @@ package com.wkl.cryptoutils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.security.Provider;
+import java.security.Security;
+import java.util.Set;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Provider[] providers = Security.getProviders();
+        for (Provider provider : providers) {
+            Set<Provider.Service> services = provider.getServices();
+            for (Provider.Service service : services) {
+                service.getType();
+                service.getAlgorithm();
+            }
+        }
     }
 }
